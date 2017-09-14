5684a1065711dd5ad1581ee85749c923982c2cfa
xunzou <xunzou@Gmail.com>
2017-09-14 21:40:54 +0800

    express router

--- app.js
+++ app.js
@@ -22,12 +22,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+//路由
+var routes = require('./routes/index.js');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-app.use('/', index);
-app.use('/users', users);
+routes(app)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,10 +39,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error');
+  res.redirect('/404');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//module.exports = router;
+module.exports = function(app) {
+	/* GET 404 page. */
+	app.get('/404', function(req, res) {
+		res.render('error', {});
+		res.sendStatus(404)
+	});
 
-module.exports = router;
+	/* GET home page. */
+	app.get('/', function(req, res, next) {
+		res.render('index', {
+			title: 'Express Study'
+		});
+	});
+
+}
